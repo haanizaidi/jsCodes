7f6cec76e77e05cc509312c19f87e567fdba46bd
haanizaidi <haanizaidi420@gmail.com>
2025-06-05 21:09:49 +0530

    first commit

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+const num=1234
+let age="sixty"
+let flag=true
+let state;
+let ego=null
+console.log(typeof ego) // object
+console.log(typeof state) // undefined 
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const registrationNumber=12206379
+let emailId="haanizaidi@gmail.com"
+let rollnum=" 62 ";
+let phonenumber="9368151533"
+
+//const registrationNumber=12206069
+emailId= "ime@gmail.com";
+console.table([registrationNumber,emailId,rollnum,phonenumber])
+ 
+
+
+// Note
+/* prefer not to use var because of issue in block scope and functional scope */
+
+
+
+
