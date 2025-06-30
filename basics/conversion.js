b3d34e7e74256b565b2301eddb60602f3960e03c
haanizaidi <haanizaidi420@gmail.com>
2025-06-30 22:33:23 +0530

    changes done

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+let score = 56
+let abc = "56abc"
+let age = "22"
+let flag= true
+let def= false
+console.log(typeof(score))
+console.log(typeof(abc))
+console.log(Number(abc))
+console.log(Number(age))
+console.log(Number(flag))
+console.log(typeof(flag)) // NaN
+console.log(Number(def))
+// "56" = 56
+// "56abc" = NaN
+// true = 1
+// false = 0
